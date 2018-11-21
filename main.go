f21d5cbce60038cfc1d1f5840dd25791ae54f676
Andrei Pago <andpago@gmail.com>
2018-11-21 12:30:24 +0300

    fix selector tests; rename MAX_TRIES flag

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	port := flag.Int("p", 8080, "Proxy port")
 	rotationTimeout := flag.Int("r", 5, "Proxy rotation timeout in minutes, 0 - disabled")
 	proxyType := flag.String("t", "http", "Proxy type http|socks")
-	maxTries := flag.Int("mt", 5, "Maximum number of requests to rufolder before giving up")
+	maxTries := flag.Int("m", 5, "Maximum number of requests to rufolder before giving up")
 
 	flag.Parse()
 
--- selector/selector_test.go
+++ selector/selector_test.go
@@ -7,12 +7,12 @@ import (
 )
 
 func TestGetNextProxyTransport(t *testing.T) {
-	_, err := selector.GetNextProxyTransport(selector.HTTP)
+	_, err := selector.GetNextProxyTransport(selector.HTTP, 5)
 	if err != nil {
 		t.Fail()
 	}
 
-	_, err = selector.GetNextProxyTransport(selector.SOCKS)
+	_, err = selector.GetNextProxyTransport(selector.SOCKS, 5)
 	if err != nil {
 		t.Fail()
 	}
